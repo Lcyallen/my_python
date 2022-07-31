a85c4a2abc19249391933ad0943c7c694c89c99e
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2022-07-31 17:29:11 +0800

    zj_a006
    用notepad++上傳

---
+++ zj_a006解二次方程.py
@@ -0,0 +1,12 @@
+a,b,c = map(int,input().split())    #三個變數的逗號間竟然不能有空格！
+D = b*b-4*a*c
+if D>0:
+  x1 = int((D**(0.5)-b)//2)
+  x2 = int((0-(D**(0.5))-b)//2)
+  print("Two different roots x1=",x1," , x2=",x2,sep='')
+
+elif D==0:
+  print("Two same roots x=",(0-b)//(2*a),sep='')
+
+else:
+  print("No real root")
