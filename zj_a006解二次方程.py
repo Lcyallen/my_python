ce8fb9d74f6314c0345e08f7379194e0e9f9713d
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2022-07-31 17:31:58 +0800

    Update zj_a006解二次方程.py

--- zj_a006解二次方程.py
+++ zj_a006解二次方程.py
@@ -1,7 +1,7 @@
 a,b,c = map(int,input().split())    #三個變數的逗號間竟然不能有空格！
 D = b*b-4*a*c
 if D>0:
-  x1 = int((D**(0.5)-b)//2)
+  x1 = int((D**(0.5)-b)//2)      #要注意利用**(0.5)求平方根後，會變成float，要再轉變成int
   x2 = int((0-(D**(0.5))-b)//2)
   print("Two different roots x1=",x1," , x2=",x2,sep='')
 
