05da76a72ac92b5ad0c5629ab0ee11f9fdea572b
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2022-08-25 10:14:18 +0800

    格式化輸出, 浮點數精確度
    利用格式化輸出設定十進位浮點數的精確度
    https://steam.oxxostudio.tw/category/python/basic/format.html#a2

---
+++ zj_d051_格式化輸出,浮點數精確度.py
@@ -0,0 +1,10 @@
+h = int(input())
+c = f'{(h-32)*5/9:.3f}' #
+print(c)
+
+'''
+1)格式化輸出用f'{}', :後面省略填充字元, 省略靠左對齊, 省略輸出寬度
+2).後面的3表示輸出小數點後3位精確度
+3)3後面的f表示輸出為10進位浮點數
+https://steam.oxxostudio.tw/category/python/basic/format.html#a2
+''' 
