7011d7a1738fc0ef25f1f4f0780a821f1594fb40
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2022-08-25 18:12:46 +0800

    Add files via upload
    1. str.capitalize()
    2. 列印生成式

---
+++ zj_c760_將str字首變大寫, 列印生成式.py
@@ -0,0 +1,7 @@
+a = input().split()
+for i in a:
+    print(i.capitalize())
+'''
+1. str.capitalize()可將str的字首轉成大寫
+2.這是我自己想到的，網路有一行解[print(i.capitalize()) for i in (input().split())]
+3.為何可以這樣寫，我還得再思考 
