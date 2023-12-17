e7eac601652e512c6f72d28853a9cca04d501c78
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2023-12-17 16:56:52 +0800

    turtle_painting04_旋轉.py
    學習引入turtle畫圖
    設定筆寬
    設定筆顏色

---
+++ turtle_painting04_旋轉.py
@@ -0,0 +1,16 @@
+import turtle
+import time
+
+t = turtle.Turtle()
+colors = ['blue','red','green','yellow','black']  #把顏色寫進list
+t.color('black')
+t.width(3)   #設定筆寬
+
+for j in range (10):
+    for i in range (4):
+        t.forward(50)
+        t.right(90)
+    t.right(36)
+    t.color(colors[j%5])
+    
+time.sleep(3)
