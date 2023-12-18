ec31fbc01e285607952b4b5605173789ebfbb31f
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2023-12-18 20:29:08 +0800

    turtle07_n個圓旋轉

---
+++ turtle07_n個圓旋轉.py
@@ -0,0 +1,18 @@
+import turtle
+import time
+
+t = turtle.Turtle()
+t.color('brown')
+t.width(2)
+t.speed(5)
+colors = ['red','orange','yellow','green','blue','purple','pink','brown']
+
+#t.penup()
+#t.goto(-150,150)
+
+for i in range(20):
+    t.circle(50)
+    t.right(18)
+    t.color(colors[(i+1)%8])
+    
+time.sleep(3)
