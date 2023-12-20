0cbd98336141ca24508ebc9a03192d765e8d7c23
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2023-12-20 11:07:33 +0800

    turtle_6邊螺旋(進化)
    多加一行t.width(i/100+1)，變得很漂亮

---
+++ turtle_6邊螺旋(進化).py
@@ -0,0 +1,24 @@
+import turtle
+import time
+
+t = turtle.Turtle()
+t.speed(0)
+
+colors =['blue','green','red','brown','pink','purple','orange','black']
+
+for i in range(100):
+    t.fd(i)
+    t.right(59)
+    t.width(i/100+1)   #看到網路上寫的這一行！有很棒的效果
+    t.color(colors[i%6])
+'''
+t.penup()
+t.fd(80)
+
+for i in range(100):
+    t.fd(i)
+    t.right(61)
+    t.width(i/100+1)
+    t.color(colors[i%6])
+'''
+time.sleep(2)
