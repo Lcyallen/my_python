f473cb05131340b9267dcf2703286acfe79017e7
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2023-12-28 15:46:02 +0800

    turtle_圓繞小圈漸變
    圓在旋轉時中間繞一個小圈

---
+++ turtle_圓繞小圈漸變.py
@@ -0,0 +1,15 @@
+import turtle as t
+import colorsys as cs
+import time
+
+t.speed(0)
+t.width(2)
+t.bgcolor('black')
+
+for i in range(150):
+    t.circle(50)
+    t.right(4)
+    t.color(cs.hsv_to_rgb(i/150,0.8,0.8))
+    t.fd(1)    #往前1格變得很重要
+    
+time.sleep(2)
