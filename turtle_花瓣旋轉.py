2dfda8974b7f1dbf259476f4ed7f0d9954f35153
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2023-12-28 16:03:50 +0800

    turtle_花瓣旋轉

---
+++ turtle_花瓣旋轉.py
@@ -0,0 +1,16 @@
+import turtle as t
+import colorsys as cs
+import time
+
+t.speed(0)
+t.width(2)
+t.bgcolor('black')
+
+for i in range(60):
+    t.circle(100,90)
+    t.lt(90)
+    t.circle(100,90)
+    t.color(cs.hsv_to_rgb(i/60,0.8,0.8))
+    t.circle(10,50)
+    
+time.sleep(2)
