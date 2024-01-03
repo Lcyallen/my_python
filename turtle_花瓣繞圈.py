fa5e0808787ca1fc5b4c1e37541c4303e36b2152
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2024-01-03 13:47:12 +0800

    turtle_花瓣繞圈
    花瓣繞著中間小圓

---
+++ turtle_花瓣繞圈.py
@@ -0,0 +1,18 @@
+import turtle as t
+import colorsys as cs
+import time
+
+t.speed(0)
+t.width(2)
+t.bgcolor('black')
+
+for i in range(60):
+    t.circle(100,90)
+    t.lt(90)
+    t.circle(100,90)
+    t.color(cs.hsv_to_rgb(i/60,0.8,0.8))
+    t.rt(180)
+    t.circle(20,50)
+    t.rt(3)    #多加這一行才能旋轉
+    
+time.sleep(2)
