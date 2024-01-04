cedebdf94c7a84e66d0e4ccd3021e18042570101
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2024-01-04 21:34:02 +0800

    turtle_葉子漸變小再旋轉

---
+++ turtle_葉子漸變小.py
@@ -0,0 +1,19 @@
+import turtle as t
+import colorsys as cs
+import time
+
+t.speed(0)
+t.bgcolor('black')
+
+for j in range (20):
+    for i in range (10):
+        t.color(cs.hsv_to_rgb(j/20,1,1))
+        t.circle(160-i*5,90)
+        t.left(90)
+        t.circle(160-i*5,90)
+        t.left(90)
+    t.right(90)
+    t.circle(25,18)
+    t.right(90)
+    
+time.sleep(5)
