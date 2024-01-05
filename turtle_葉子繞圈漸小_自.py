0edc9208474031ca5074cacda36e02178c1f6c67
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2024-01-05 22:05:13 +0800

    turtle_葉子繞圈漸小_自
    1.自己嘗試出來的成果
    2.每片葉子顏色相同
    3.中間有小圈

---
+++ turtle_葉子繞圈漸小_自.py
@@ -0,0 +1,18 @@
+import turtle as t
+import colorsys as cs
+import time
+
+t.speed(0)
+t.bgcolor('black')
+
+for j in range (10):
+    for i in range (15):
+        t.color(cs.hsv_to_rgb(i/15,1,1))
+        t.circle(150-j*3,90)
+        t.lt(90)
+        t.circle(150-j*3,90)
+        t.rt(90)
+        t.circle(25,24)
+        t.rt(180)
+
+time.sleep(2)
