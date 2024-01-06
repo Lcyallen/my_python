5064a0ecf543f388b998ac75f889d512b808e993
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2024-01-06 16:31:19 +0800

    turtle_葉先漸小再繞圈
    1.終於試成功了！
    2.先利用慢速觀察箭頭方向與結果，找到問題後再嘗試！

---
+++ turtle_葉先漸小再繞圈.py
@@ -0,0 +1,18 @@
+import turtle as t
+import colorsys as cs
+import time
+
+t.speed(0)
+t.bgcolor('black')
+
+for j in range (15):
+    for i in range (8):
+        t.color(cs.hsv_to_rgb(i/8,1,1))
+        t.circle(150-i*5,90)
+        t.lt(90)
+        t.circle(150-i*5,90)
+        t.lt(90)    #左轉90度才能回原方向繼續畫葉子
+    t.rt(180)   #畫完葉子方向向右，此時須先向後轉，往左前進
+    t.circle(25,24) #走小圈
+    t.rt(180)   #這個是關鍵！
+time.sleep(2)
