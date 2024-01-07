36e96c9c963d6f409a7c76cb2f7d36612979f00e
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2024-01-07 10:02:35 +0800

    turtle_黃金螺旋
    1.自己給自己的題目挑戰
    2.在變數a, b, c的交換與位置調整很多次
    3.每畫完一次(90度)就變換一次顏色

---
+++ turtle_黃金螺旋.py
@@ -0,0 +1,19 @@
+import turtle as t
+import colorsys as cs
+import time
+
+t.speed()
+t.bgcolor('black')
+
+a = 0
+b = 1+a
+
+for i in range (11):
+    t.color(cs.hsv_to_rgb(i/11,1,1))
+    c = a+b
+    t.width(i)
+    t.circle(c,90)
+    a = b
+    b = c
+    
+time.sleep(2)
