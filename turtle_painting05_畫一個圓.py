fd0597680ebf4e5b5c0bdf0db01e6fbdf54dfb6a
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2024-01-11 09:42:02 +0800

    Update turtle_painting05_畫一個圓.py

--- turtle_painting05_畫一個圓.py
+++ turtle_painting05_畫一個圓.py
@@ -1,10 +1,8 @@
-import turtle
-import time
+import turtle as t
 
-t = turtle.Turtle()
 #colors = ['black','pink','blue','green','brown']  #把顏色寫進list
 t.color('brown')
 t.width(3)   #設定筆寬
 t.circle(50)
     
-time.sleep(3)
+t.done()
