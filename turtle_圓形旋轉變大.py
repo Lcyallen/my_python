37a76060b7de74971dda120abe9ffa0edeaefda1
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2024-01-11 09:28:02 +0800

    Update turtle_圓形旋轉變大.py

--- turtle_圓形旋轉變大.py
+++ turtle_圓形旋轉變大.py
@@ -1,7 +1,5 @@
-import turtle
-import time
+import turtle as t
 
-t = turtle.Turtle()
 t.speed(0)
 t.width(2)
 colors =['blue','green','red','brown','pink','purple','orange','black']
@@ -12,4 +10,4 @@ for i in range(1,8):
         t.right(45)
     t.color(colors[i%8])
 
-time.sleep(1)
+t.done()
