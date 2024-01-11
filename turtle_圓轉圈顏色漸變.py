8f489e66537f6067b178c8b1ce548cefe7c3bf02
Allen Ling <87615269+Lcyallen@users.noreply.github.com>
2024-01-11 09:30:42 +0800

    Update turtle_圓轉圈顏色漸變.py

--- turtle_圓轉圈顏色漸變.py
+++ turtle_圓轉圈顏色漸變.py
@@ -1,6 +1,5 @@
 import turtle as t
 import colorsys as cs
-import time
 
 t.speed(0)
 t.width(2)
@@ -11,4 +10,4 @@ for i in range(100):
     t.right(4)
     t.color(cs.hsv_to_rgb(i/100,1,0.8))
     
-time.sleep(2)
+t.done()
